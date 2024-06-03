1a8ffd9757c1293da7463b520ed5a4d421208001
Aldo Maria Vizcaino <aldo.vizcaino87@gmail.com>
2024-06-03 19:31:39 +0700

    remove generic definitions, encourage to use direnv

--- .zsh/gitlab.sh
+++ .zsh/gitlab.sh
@@ -1,23 +1,13 @@
 #!/bin/env zsh
 
-export GITLAB_Nomanod=$(pass show Work/gitlab.com/group_nomanod)
-export GITLAB_Awrora=$(pass show Work/gitlab.com/group_awrora)
-export GITLAB_Grallamadrid=$(pass show Work/gitlab.com/group_grallamadrid)
-export GITLAB_ingeniero=$(pass show Work/gitlab.com/group_ingeniero)
-export GITLAB_inthependiente=$(pass show Work/gitlab.com/group_inthependiente)
-# The generic group definition is to avoid memorization.
-# On the group folder, using direnv (.envrc) there will be a definition for
-# GITLAB_GROUP.
-# For example:
-# export GITLAB_GROUP=${GITLAB_ingeniero}
-
+# The group and name variables have to be defined inside the group folder
 function gitlab-create(){
     gitlab project create --name $argv[1] --namespace ${GITLAB_GROUP}
     echo git@gitlab.com:${GITLAB_NAME}/$argv[1].git
-    read -p "Do you want to clone it at ${GITLAB_PATH}/$argv1?" -n 1 -r
+    read -p "Do you want to clone it at ./$argv1?" -n 1 -r
     echo    # (optional) move to a new line
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-       git clone git@gitlab.com:${GITLAB_NAME}/$argv[1].git ${GITLAB_PATH}/$argv[1]
+       git clone git@gitlab.com:${GITLAB_NAME}/$argv[1].git ./$argv[1]
     fi
 }
 
